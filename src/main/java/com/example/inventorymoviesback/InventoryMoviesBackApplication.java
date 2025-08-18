fe176626a51af72a02406ddd2a91a1d7d08b6226
Cristian Cajiao <cristiancajiaos@outlook.com>
2025-08-18 12:12:12 -0400

    Primer commit

---
+++ src/main/java/com/example/inventorymoviesback/InventoryMoviesBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.inventorymoviesback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryMoviesBackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InventoryMoviesBackApplication.class, args);
+  }
+
+}
